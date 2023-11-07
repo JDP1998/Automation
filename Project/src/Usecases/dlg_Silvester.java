b1462f22e23e895adfa2c68869758d69ff3b1409
Johannes Portzehl <johannes.portzehl@sogeti.com>
2023-11-07 16:46:06 +0100

    Fix Silvester

--- Project/src/Usecases/dlg_Silvester.java
+++ Project/src/Usecases/dlg_Silvester.java
@@ -1,13 +1,15 @@
 package Usecases;
 
+import java.util.Calendar;
 import java.util.GregorianCalendar;
 
 public class dlg_Silvester {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		int year = Calendar.getInstance().get(Calendar.YEAR);
 		GregorianCalendar heute = new GregorianCalendar();
-		GregorianCalendar weihnachten = new GregorianCalendar(2022, 12, 31);
+		GregorianCalendar weihnachten = new GregorianCalendar(year, 12, 31);
 		long differenz = weihnachten.getTimeInMillis()-heute.getTimeInMillis();
 		int tage;
 		tage=(int)(differenz/(1000*60*60*24))-30;
