f39602f6c60726a07150eb46ac5783a222ff1f93
Johannes Portzehl <johannes.portzehl@sogeti.com>
2023-11-20 14:57:58 +0100

    Fix Random

--- Project/src/Usecases/dlg_Random.java
+++ Project/src/Usecases/dlg_Random.java
@@ -7,17 +7,19 @@ public class dlg_Random {
 		 long counter=0,start,end,time;
          int zahl=1000001,zufall;
          start=System.currentTimeMillis();
-         zufall=(int)(Math.random()*100)+1;
+         zufall=(int)(Math.random()*1000)+1;
          System.out.println("Die gesuchte Zahl ist: "+zufall);
          while (zahl!=zufall) {
-        	 zahl=(int)(Math.random()*100)+1;
+        	 zahl=(int)(Math.random()*1000)+1;
         	 System.out.println("Die Zufallszahl ist: "+zahl);
         	 counter++;
         	 Thread.sleep(2000);
          }
          end=System.currentTimeMillis();
          time=end-start;
-         System.out.println("Es ist beendet. Es hat "+time+" Milliskeunden gedauert.");
+         time=time/1000;
+         time=time/60;
+         System.out.println("Es ist beendet. Es hat "+time+" Minuten gedauert.");
          System.out.println("Die Schleife musste "+counter+ " mal durchlaufen werden.");
 	}
 
